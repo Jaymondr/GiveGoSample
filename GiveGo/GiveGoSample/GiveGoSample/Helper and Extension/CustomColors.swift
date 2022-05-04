52c0cfc5d8e3e09068f854b8338f7343715fbf2b
Jaymond Richardson <82681685+Jaymondr@users.noreply.github.com>
2022-05-03 23:04:44 -0600

    Add files via upload

---
+++ GiveGo/GiveGoSample/GiveGoSample/Helper and Extension/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  GiveGoSample
+//
+//  Created by Jaymond Richardson on 5/3/22.
+//
+
+import UIKit
+
+struct CustomColors {
+    
+    static let customBlue = UIColor(named: "customBlue")
+    static let customBackground = UIColor(named: "customBackground")
+    static let pageIndicatorTint = UIColor(named: "pageIndicatorTint")
+
+}
